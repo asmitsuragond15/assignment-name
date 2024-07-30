fdff178b68cb173f858a1667b4c469a3b4006f78
Asmit Suragond <suragondasmit15@gmail.com>
2024-07-30 14:31:06 +0530

    Assign1 completed

---
+++ assign1.java
@@ -0,0 +1,33 @@
+import java.util.*;
+        public class assign1
+        {
+
+
+            public void reactagle() {
+
+                System.out.println("This Area of Reactagle");
+                double a,b;
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter the length of the rectangle: ");
+               double length = scanner.nextDouble();
+
+                System.out.print("Enter the breadth of the rectangle: ");
+                double breadth = scanner.nextDouble();
+
+                double area = length * breadth;
+                double perimeter = 2 * (length + breadth);
+
+        
+                 System.out.println("Area of the rectangle: " + area);
+                 System.out.println("Perimeter of the rectangle: " + perimeter);
+
+            }
+        
+            public static void main(String[] args)
+            {
+                assign1 a1=new assign1();
+                a1.reactagle();
+
+            
+            }
+        }
